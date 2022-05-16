b4a465e4f8807b7656606106e5a20008df744807
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2022-05-16 13:25:14 +0530

    first commit

---
+++ src/views/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Hero from "../components/homepage/Hero";
+import Header from "../components/homepage/Header";
+import Faq from "../components/homepage/Faq";
+import Team from "../components/homepage/Team";
+import AboutUs from "../components/homepage/AboutUs";
+import OurStory from "../components/homepage/OurStory";
+import Footer from "../components/homepage/Footer";
+import Roadmap from "../components/homepage/Roadmap";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <OurStory />
+      <AboutUs />
+      <Roadmap />
+      <Team />
+      <Faq />
+      <Footer />
+    </div>
+  );
+};
+export default Home;
